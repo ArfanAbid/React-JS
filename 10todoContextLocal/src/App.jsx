616f82302d1b3fbe63fd764f599a91a2e5066f96
Arfan Abid <iffionex111@gmail.com>
2024-07-11 18:03:27 +0500

    Done Todo using ContextApi & LocalStorage

--- 10todoContextLocal/src/App.jsx
+++ 10todoContextLocal/src/App.jsx
@@ -1,5 +1,6 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import {TodoProvider} from "./context/index"
+import { TodoForm, TodoItem } from './components'
 
 
 function App() {
@@ -9,7 +10,7 @@ function App() {
 
   const addTodo =(todo) => {
         // setTodos(todo) // this is wrong it will delete all the previously added tasks so we use a callback here
-        setTodos((prev) =>[{id:Date.now(), ...todo}, ...prev]) // maintain previous tasks and append them 
+        setTodos((prev) =>[{id:Date.now(), ...todo}, ...prev]) // maintain previous tasks and append them  Note: ... is spread operator
   }
 
   const updateTodo =(id,todo) => {
@@ -20,8 +21,23 @@ function App() {
     setTodos((prev)=> prev.filter((todo)=> todo.id !== id)) // this will give us new array with only id not present in it. so it will filter all the elements except id
   }
 
+  const toggleComplete=(id) => {
+    setTodos((prev) => prev.map((prevTodo) => prevTodo.id === id ?{...prevTodo,completed: !prevTodo.completed} : prevTodo )) // if id matches with the previous task id then ...prevTodo mean remain uncnhange all the elements but completed: mean change only this element property or override it 
+  }
 
+  // LocalStorage working
 
+  useEffect(() => {
+    const todos=JSON.parse(localStorage.getItem("todos"))
+
+    if(todos && todos.length > 0) {
+      setTodos(todos)
+    }
+  },[])
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos))
+  },[todos])
 
   return (
     < TodoProvider value={{todos,addTodo,updateTodo,deleteTodo,toggleComplete}}>
@@ -30,9 +46,15 @@ function App() {
               <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
               <div className="mb-4">
                   {/* Todo form goes here */} 
+                  <TodoForm/>
               </div>
               <div className="flex flex-wrap gap-y-3">
                   {/*Loop and Add TodoItem here */}
+                  {todos.map((todo)=>(
+                    <div key={todo.id} className='w-full'> 
+                      <TodoItem todo={todo}/>
+                    </div>
+                  ))}
               </div>
           </div>
       </div>    
